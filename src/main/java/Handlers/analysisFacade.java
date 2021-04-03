4f68ca76267e57db82c7db55704c6e902d537d97
Navjeeven Singh Mann <nmann29@uwo.ca>
2021-04-03 18:48:50 -0400

    Import Countries

--- src/main/java/Handlers/analysisFacade.java
+++ src/main/java/Handlers/analysisFacade.java
@@ -3,24 +3,12 @@ package Handlers;
 import java.util.Map;
 
 public class analysisFacade {
-	countryObject currentCountry = new countryObject();
+
 
 
 	public analysisFacade(String country,String dateRange){
 
-		currentCountry.setTotalPop(getMap(country,"SP.POP.TOTL",dateRange));
-		currentCountry.setCarbonDioxideEmissions(getMap(country,"EN.ATM.CO2E.PC",dateRange));
-		currentCountry.setAirPollution(getMap(country,"EN.ATM.PM25.MC.M3",dateRange));
-		currentCountry.setForestArea(getMap(country,"AG.LND.FRST.ZS",dateRange));
-		currentCountry.setEnergyUse(getMap(country,"EG.USE.PCAP.KG.OE",dateRange));
-		currentCountry.setGdpPerCapita(getMap(country,"NY.GDP.PCAP.CD",dateRange));
-		currentCountry.setHospitalBeds(getMap(country,"SH.MED.BEDS.ZS",dateRange));
-		currentCountry.setGovernmentExpenditure(getMap(country,"SE.XPD.TOTL.GD.ZS",dateRange));
-		currentCountry.setMaternalRatio(getMap(country,"SH.STA.MMRT",dateRange));
-		currentCountry.setHealthExpenditureCapita(getMap(country,"SH.XPD.CHEX.PC.CD",dateRange));
-		currentCountry.setHealthExpenditurePercentage(getMap(country,"SH.XPD.CHEX.GD.ZS",dateRange));
-		currentCountry.setInfantRatio(getMap(country,"SP.DYN.IMRT.IN",dateRange));
-		printAll(currentCountry);
+		
 	}
 
 	public Map<Integer, Double> getMap(String country, String analysis, String dateRange){
@@ -31,10 +19,7 @@ public class analysisFacade {
 		return integerDoubleMap;
 	}
 
-	public void printAll(countryObject country){
-		System.out.println("Carbon Dioxide Emmisions"+country.getCarbonDioxideEmissions());
-		System.out.println("GDP per Capita"+country.getGdpPerCapita());
-	}
+
 
 
 
